f0c862768fa72a01f136814460e73505e48f6a80
Damola Adewunmi <damolasd@gmail.com>
2020-10-27 14:22:54 +0100

    made changes

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const apiRouter = require('./api');
 
 const cors = require('cors');
-const keys = require('./config/secret');
+// const keys = require('./config/secret');
 
 
 const server = express();
 server.use(express.json());
 server.use('/api', apiRouter);
-server.use(
-  cors()
-);
+server.use(cors());
 
 server.get('/', async (_, res) => {
     res.status(200).json(`API endpoints exposed at /api`);
